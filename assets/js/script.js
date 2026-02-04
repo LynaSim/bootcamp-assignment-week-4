74914814bef5a09c8047d7c8e4d8e34d46e0c0ce
LynaSim <lyna.cloud@gmail.com>
2026-02-04 23:56:43 +0000

    Edited scripts.js to create a new list item on button click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
-var addNewTaskDiv = document.querySelector('.add-new-task');
+var addButton = document.getElementById('add-button');
 var taskInput = document.getElementById('task-name');
+console.log(taskInput);
 
-addNewTaskDiv.addEventListener('click', function (event) {
+addButton.addEventListener('click', function (event) {
 
     var newTask = taskInput.value;
     var newDivEl = document.createElement("div");
     newDivEl.className = "list-item";
     newDivEl.textContent = newTask;
+    console.log(newDivEl);
     var mainContentEl = document.querySelector(".list-container");
     mainContentEl.appendChild(newDivEl);
+    var someHTML = `<div class="list-item">
+                    <div>
+                        <input type="checkbox" id="task" name="task" value="task" unchecked />
+                        <label for="task">${newTask}</label>
+                        <button type="button" class="delete-task">Delete</button>
+                    </div>
+                </div>`;
+    newDivEl.outerHTML = someHTML;
+    taskInput.value = "";
 
 });
 
