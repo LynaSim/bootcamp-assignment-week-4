1c4cf17a468a1747e18aee2cf2c9c902a601f905
LynaSim <lyna.cloud@gmail.com>
2026-02-05 14:46:38 +0000

    Added validation for input and alert for empty value.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+Week 4 assignment.
+
 Create an interactive TODO list application.
+
+I created a mini form that lets the user:
+input the name of the task
+add it to a list of tasks
+check a box to mark the task as done
+delete a task by way of clicking a button
+
+Additionally the form will block empty an empty entry and prompt the user to type a value, by way of an alert box.
+
+Link to live page:
+https://lynasim.github.io/bootcamp-assignment-week-4/
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,47 +2,49 @@ var addButton = document.getElementById('add-button');
 var taskInput = document.getElementById('task-name');
 console.log(taskInput);
 
+
 addButton.addEventListener('click', function (event) {
     //Creates a new task on every click
     var newTask = taskInput.value;
-    var newDivEl = document.createElement("div");
-    newDivEl.className = "list-item";
-    newDivEl.textContent = newTask;
-    console.log(newDivEl);
-    var mainContentEl = document.querySelector(".list-container");
-    mainContentEl.appendChild(newDivEl);
-    var someHTML = `<div class="list-item">
+
+    //Checks the value is not empty
+    if (newTask) {
+
+        var newDivEl = document.createElement("div");
+        newDivEl.className = "list-item";
+        newDivEl.textContent = newTask;
+        console.log(newDivEl);
+        var mainContentEl = document.querySelector(".list-container");
+        mainContentEl.appendChild(newDivEl);
+        var someHTML = `<div class="list-item">
                     <div>
-                        <input type="checkbox" id="task" name="task" value="task" unchecked />
+                        <input type="checkbox" name="task" value="task" unchecked />
                         <label for="task">${newTask}</label>
                         <button type="button" class="delete-button">Delete</button>
                     </div>
                 </div>`;
-    newDivEl.outerHTML = someHTML;
-    taskInput.value = "";
+        newDivEl.outerHTML = someHTML;
+        taskInput.value = "";
 
-    // Adds an eventlistener to each delete-button created
-    // var mainContentEl = document.querySelector(".list-container");
-    var deleteButtons = document.querySelectorAll(".delete-button");
-    console.log(deleteButtons);
-    deleteButtons.forEach(function(button) {
-        button.addEventListener('click', function (event) {
-            var nodeToDelete = button.parentNode;
-            console.log(nodeToDelete);
-            nodeToDelete.remove();
+        // Adds an eventlistener to each delete-button created
+        var deleteButtons = document.querySelectorAll(".delete-button");
+        console.log(deleteButtons);
+        deleteButtons.forEach(function (button) {
+            button.addEventListener('click', function (event) {
+                var nodesToDelete = button.parentNode;
+                console.log(nodesToDelete);
+                nodesToDelete.remove();
+            });
         });
-    });
+
+    } else {
+        alert("Please type something.");
+    };
 
 });
 
 
 
-
-
-// deleteButton.addEventListener('click', function (event) {
-//     var nodesToDelete = deleteButton.parentNode;
-//     console.log(nodesToDelete);
-// });
 
 
 
