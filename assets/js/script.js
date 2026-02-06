0d475da5f6836a764e0bdcdca7fa03f17b3c8633
LynaSim <lyna.cloud@gmail.com>
2026-02-06 09:49:51 +0000

    Added the fumctionnality for adding a task by simply pressing 'enter' from the input field.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,55 @@ var addButton = document.getElementById('add-button');
 var taskInput = document.getElementById('task-name');
 console.log(taskInput);
 
+//Handling event of press enter in input field
+taskInput.addEventListener('keydown', function (event) {
+    console.log(event);
+
+    if (event.key === 'Enter') {
+
+        //Creates a new task on pressing 'enter' key
+        var newTask = taskInput.value;
+
+        //Checks the value is not empty
+        if (newTask) {
+
+            var newDivEl = document.createElement("div");
+            newDivEl.className = "list-item";
+            newDivEl.textContent = newTask;
+            console.log(newDivEl);
+            var mainContentEl = document.querySelector(".list-container");
+            mainContentEl.appendChild(newDivEl);
+            var someHTML = `<div class="list-item">
+                    <div>
+                        <input type="checkbox" name="task" value="task" unchecked />
+                        <label for="task">${newTask}</label>
+                        <button type="button" class="delete-button">Delete</button>
+                    </div>
+                </div>`;
+            newDivEl.outerHTML = someHTML;
+            taskInput.value = "";
+
+            // Adds an eventlistener to each delete-button created
+            var deleteButtons = document.querySelectorAll(".delete-button");
+            console.log(deleteButtons);
+            deleteButtons.forEach(function (button) {
+                button.addEventListener('click', function (event) {
+                    var nodesToDelete = button.parentNode;
+                    console.log(nodesToDelete);
+                    nodesToDelete.remove();
+                });
+            });
+
+        } else {
+            alert("Please type something.");
+        };
+
+
+    }
+
+})
+
+
 
 addButton.addEventListener('click', function (event) {
     //Creates a new task on every click
